038d72f69b13d0b08c525f893ffb7bdb9ebb0787
Abbas Tailor <abbastailor@outlook.com>
2021-08-04 01:35:06 +0530

    BugFix

--- background.js
+++ background.js
@@ -93,7 +93,7 @@ async function verifyLogin(reqData , api){
             error:(err)=>{
                 console.log("error ajax get");
                 console.log(err);
-                return err;
+                reject (err);
             }
         });
     });
